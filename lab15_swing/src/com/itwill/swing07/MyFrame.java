f3b1ea747392345a1b402e5e94e8ad8f634266ea
woodstock0513 <judydayoung@gmail.com>
2024-04-18 09:50:32 +0900

    little modification_dispose_on_close

--- lab15_swing/src/com/itwill/swing07/MyFrame.java
+++ lab15_swing/src/com/itwill/swing07/MyFrame.java
@@ -37,7 +37,7 @@ public class MyFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public void initialize() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
